b959b985f8ba06ba92d76b2c55a938c9ac200fbc
François Gauthier-Clerc <francois.gauthier-clerc@insa-rennes.fr>
2018-05-26 22:48:09 +0200

    move cuda file from root into directory "cuda_lib".

---
+++ cuda_lib/matrix.h
@@ -0,0 +1,22 @@
+#ifndef _MATRIX_H_
+#define _MATRIX_H_
+
+#include <iostream>
+#include <cuda.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include  <cstdlib>
+#include <ctime>
+#define DIM 12
+
+
+
+void matrix_multiplication(double * a, double * b, double * c, int dim);
+
+
+
+
+
+
+#endif
