31027e39a86df66d98b9a359d27a3cfb4095d10c
François Gauthier-Clerc <francois.gauthier-clerc@insa-rennes.fr>
2018-05-31 10:47:25 +0200

    Add a nvidia constant storage header file.

---
+++ include/CUDA_CONSTANT.h
@@ -0,0 +1,10 @@
+//
+// Created by francois on 30/05/18.
+//
+
+#ifndef _CUDA_MATRIX_LIB_CONSTANT_H
+#define _CUDA_MATRIX_LIB_CONSTANT_H
+
+#define MAX_THREAD_BY_BLOCKS 1024
+
+#endif //_CUDA_MATRIX_LIB_CONSTANT_H
