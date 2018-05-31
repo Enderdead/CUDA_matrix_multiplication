7e29c4aef7ca602b9a22872676631f53e8982de2
François Gauthier-Clerc <francois.gauthier-clerc@insa-rennes.fr>
2018-05-31 10:47:56 +0200

    Add Example projet using CUDA library.

---
+++ example/main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by francois on 30/05/18.
+//
+
+#include <stdlib.h>
+#include <iostream>
+#include "../include/cuvector.h"
+
+
+using namespace std;
+
+int main(void)
+{
+    CuVector<int> t(3);
+    return 0;
+}
